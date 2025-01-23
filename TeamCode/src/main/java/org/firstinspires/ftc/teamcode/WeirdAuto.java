4478f8f21b0a74f66a87f010a7be7cdee9778dbb
crazyguy2653 <111104070+crazyguy2653@users.noreply.github.com>
2025-01-23 17:13:57 -0600

    Update WeirdAuto.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WeirdAuto.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/WeirdAuto.java
@@ -41,7 +41,7 @@ import org.firstinspires.ftc.robotcore.external.navigation.DistanceUnit;
 
 @Autonomous(name="WeirdAuto", group="Linear OpMode")
 public class WeirdAuto extends LinearOpMode {
-    SparkFunOTOS myOtos;
+    private SparkFunOTOS myOtos;
     private DcMotor frontLeftDrive = null;
     private DcMotor frontRightDrive = null;
     private DcMotor rearLeftDrive = null;
@@ -54,7 +54,6 @@ public class WeirdAuto extends LinearOpMode {
         frontRightDrive = hardwareMap.get(DcMotor.class, "front_right_drive");
         rearLeftDrive = hardwareMap.get(DcMotor.class, "rear_left_drive");
         rearRightDrive = hardwareMap.get(DcMotor.class, "rear_right_drive");
-
         frontLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
         frontRightDrive.setDirection(DcMotorSimple.Direction.FORWARD);
         rearLeftDrive.setDirection(DcMotorSimple.Direction.REVERSE);
@@ -62,12 +61,46 @@ public class WeirdAuto extends LinearOpMode {
         frontLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         frontRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         rearLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
-        rearLeftDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        rearRightDrive.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
         configureOtos();
         myOtos.resetTracking();
         waitForStart();
-        moveRobot(10,0,0,0.1, 0.5, 1.0);
-        sleep(100000);
+
+        SparkFunOTOS.Pose2D pos = myOtos.getPosition();
+
+        telemetry.addLine("this works");
+
+        while(valueNotRoughlyEqual(pos.x, 5, 0.5) && opModeIsActive()){
+            rearRightDrive.setPower(1);
+            rearLeftDrive.setPower(1);
+            frontLeftDrive.setPower(1);
+            frontRightDrive.setPower(1);
+            pos = myOtos.getPosition();
+            telemetry.addLine("you're in the while loop");
+            telemetry.addData("x:",pos.x);
+            telemetry.update();
+        }
+
+        while (opModeIsActive()){
+            pos = myOtos.getPosition();
+            telemetry.addData("x:",pos.x);
+            telemetry.addData("y:",pos.y);
+            telemetry.addData("h:",pos.h);
+            telemetry.addData("opModeIsActive",opModeIsActive());
+            telemetry.update();
+            break;
+
+        }
+
+        telemetry.addLine("this also works");
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+        rearLeftDrive.setPower(0);
+        rearRightDrive.setPower(0);
+        telemetry.addLine("end of the line");
+        telemetry.update();
+        sleep(10000);
+        //moveRobot(10,0,0,0.1, 0.5, 1.0);
     }
 
     public void moveRobot(double x,double y,double h,double speed,double posTolerance,double headingTolerance){
@@ -137,11 +170,20 @@ public class WeirdAuto extends LinearOpMode {
             telemetry.addData("rearRightPower",rearRightPower);
             telemetry.update();
         }
+        frontLeftDrive.setPower(0);
+        frontRightDrive.setPower(0);
+        rearLeftDrive.setPower(0);
+        rearRightDrive.setPower(0);
     }
 
 
     public boolean valueNotRoughlyEqual(double value, double goal, double tolerance) {
-        return !(value >= goal - tolerance) || !(value <= goal + tolerance);
+        if ((value > goal + tolerance) && (value < goal - tolerance)){
+            return true;
+        }
+        else {
+            return false;
+        }
     }
 
     public double bangBangController(double goalPos, double currentPos,double speed) {
@@ -160,73 +202,24 @@ public class WeirdAuto extends LinearOpMode {
         telemetry.addLine("Configuring OTOS...");
         telemetry.update();
 
-        // Set the desired units for linear and angular measurements. Can be either
-        // meters or inches for linear, and radians or degrees for angular. If not
-        // set, the default is inches and degrees. Note that this setting is not
-        // persisted in the sensor, so you need to set at the start of all your
-        // OpModes if using the non-default value.
         // myOtos.setLinearUnit(DistanceUnit.METER);
         myOtos.setLinearUnit(DistanceUnit.INCH);
         // myOtos.setAngularUnit(AnguleUnit.RADIANS);
         myOtos.setAngularUnit(AngleUnit.DEGREES);
 
-        // Assuming you've mounted your sensor to a robot and it's not centered,
-        // you can specify the offset for the sensor relative to the center of the
-        // robot. The units default to inches and degrees, but if you want to use
-        // different units, specify them before setting the offset! Note that as of
-        // firmware version 1.0, these values will be lost after a power cycle, so
-        // you will need to set them each time you power up the sensor. For example, if
-        // the sensor is mounted 5 inches to the left (negative X) and 10 inches
-        // forward (positive Y) of the center of the robot, and mounted 90 degrees
-        // clockwise (negative rotation) from the robot's orientation, the offset
-        // would be {-5, 10, -90}. These can be any value, even the angle can be
-        // tweaked slightly to compensate for imperfect mounting (eg. 1.3 degrees).
         SparkFunOTOS.Pose2D offset = new SparkFunOTOS.Pose2D(0, 0, 0);
         myOtos.setOffset(offset);
 
-        // Here we can set the linear and angular scalars, which can compensate for
-        // scaling issues with the sensor measurements. Note that as of firmware
-        // version 1.0, these values will be lost after a power cycle, so you will
-        // need to set them each time you power up the sensor. They can be any value
-        // from 0.872 to 1.127 in increments of 0.001 (0.1%). It is recommended to
-        // first set both scalars to 1.0, then calibrate the angular scalar, then
-        // the linear scalar. To calibrate the angular scalar, spin the robot by
-        // multiple rotations (eg. 10) to get a precise error, then set the scalar
-        // to the inverse of the error. Remember that the angle wraps from -180 to
-        // 180 degrees, so for example, if after 10 rotations counterclockwise
-        // (positive rotation), the sensor reports -15 degrees, the required scalar
-        // would be 3600/3585 = 1.004. To calibrate the linear scalar, move the
-        // robot a known distance and measure the error; do this multiple times at
-        // multiple speeds to get an average, then set the linear scalar to the
-        // inverse of the error. For example, if you move the robot 100 inches and
-        // the sensor reports 103 inches, set the linear scalar to 100/103 = 0.971
         myOtos.setLinearScalar(0.9961);
         myOtos.setAngularScalar(0.9899);
 
-        // The IMU on the OTOS includes a gyroscope and accelerometer, which could
-        // have an offset. Note that as of firmware version 1.0, the calibration
-        // will be lost after a power cycle; the OTOS performs a quick calibration
-        // when it powers up, but it is recommended to perform a more thorough
-        // calibration at the start of all your OpModes. Note that the sensor must
-        // be completely stationary and flat during calibration! When calling
-        // calibrateImu(), you can specify the number of samples to take and whether
-        // to wait until the calibration is complete. If no parameters are provided,
-        // it will take 255 samples and wait until done; each sample takes about
-        // 2.4ms, so about 612ms total
         myOtos.calibrateImu();
 
-        // Reset the tracking algorithm - this resets the position to the origin,
-        // but can also be used to recover from some rare tracking errors
         myOtos.resetTracking();
 
-        // After resetting the tracking, the OTOS will report that the robot is at
-        // the origin. If your robot does not start at the origin, or you have
-        // another source of location information (eg. vision odometry), you can set
-        // the OTOS location to match and it will continue to track from there.
         SparkFunOTOS.Pose2D currentPosition = new SparkFunOTOS.Pose2D(0, 0, 0);
         myOtos.setPosition(currentPosition);
 
-        // Get the hardware and firmware version
         SparkFunOTOS.Version hwVersion = new SparkFunOTOS.Version();
         SparkFunOTOS.Version fwVersion = new SparkFunOTOS.Version();
         myOtos.getVersionInfo(hwVersion, fwVersion);
