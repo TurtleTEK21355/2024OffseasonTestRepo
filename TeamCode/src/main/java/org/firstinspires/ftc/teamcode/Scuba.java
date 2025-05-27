b3697e50926ff50a792bb185ae297ff4526ab684
TEKLAB-05 <turtletek@CareerTek2017.local>
2025-05-27 17:55:10 -0500

    get rid of useless garbage

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/Scuba.java
@@ -0,0 +1,12 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.hardware.DcMotor;
+public class Scuba {
+    private DcMotor frontLeftMotor;
+    private DcMotor frontRightMotor;
+    private DcMotor rearLeftMotor;
+    private DcMotor rearRightMotor;
+    private void DriveTrainControl(double drive, double strafe, double control){
+        
+    }
+}
