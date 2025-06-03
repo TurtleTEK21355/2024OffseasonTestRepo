e751ef391b0a1e374aa3098ba4c6887a3e293662
TEKLAB-05 <turtletek@CareerTek2017.local>
2025-06-03 17:01:48 -0500

    Update NewOpMode.java

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewOpMode.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/NewOpMode.java
@@ -5,7 +5,8 @@ import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
 @TeleOp(name="NewOpMode", group="Iterative OpModes")
 public class NewOpMode extends OpMode {
     public void init(){
-
+        Scuba Scuba1 = new Scuba();
+        Scuba1.driveTrainControl();
     }
     public void loop(){
 
