2599067abf583bd966abbd65ca13bc5055eeca0c
TEKLAB-05 <turtletek@CareerTek2017.local>
2025-06-03 19:20:27 -0500

    asdfasdfasdfasdfasdfasdfasdfasdf

--- TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Scuba2Arm.java
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/robot/Scuba2Arm.java
@@ -1,4 +1,6 @@
 package org.firstinspires.ftc.teamcode.robot;
 
-public class Scuba2Arm {
+public class Scuba2Arm extends Arm{
+    Scuba2HorizontalSlide linearActuator;
+    Scuba2VerticalSlide viperSlides;
 }
